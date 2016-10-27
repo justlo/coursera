e0837ada17b45051e484f5b349a4fb4a6bcfb14d
Justine <justinelo010@gmail.com>
2016-10-27 10:05:46 -0400

    commit portfolio site v1.3

---
+++ Portfolio_4.3/js/app.js
@@ -0,0 +1,15 @@
+// scrollNav
+// data-spy="scroll"
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
+// var sidebar = document.getElementById('sidebar');
+// Stickyfill.add(sidebar);
+// $(".sticky_column").stick_in_parent({offset_top: 50});
+
+$(".sticky_column").stick_in_parent({
+    offset_top: 200
+
+});
